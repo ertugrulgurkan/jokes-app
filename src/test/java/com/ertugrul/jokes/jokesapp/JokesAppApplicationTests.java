ae89dbd8a78849bf150611d6fa7834e0ef1ff6ec
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-06-23 20:56:06 +0300

    Initial commit

---
+++ src/test/java/com/ertugrul/jokes/jokesapp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ertugrul.jokes.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
